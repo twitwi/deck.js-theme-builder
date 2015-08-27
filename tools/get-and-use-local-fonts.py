60c35842d6d32eb2a65a643683ec689a5ee090b4
Rémi Emonet <remi.emonet@reverse--com.heeere>
2015-08-27 18:58:02 +0200

    switched to python3

--- tools/get-and-use-local-fonts.py
+++ tools/get-and-use-local-fonts.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-
 import re
-import urllib2
+import urllib.request
 
 input  = "_fontbox.scss.in"
 output = "_fontbox.scss"
 
 def readURL(url):
-    return urllib2.urlopen(urllib2.Request(url, None, {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})).read()
+    r = urllib.request.urlopen(urllib.request.Request(url, None, {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'}))
+    return r.read()
 
 def writeFile(path, content):
-    with open(path, 'w') as f:
+    with open(path, 'wb') as f:
         f.write(content)
 
 with open(input, 'r') as f, open(output, 'w') as outputF:
@@ -18,7 +18,7 @@ with open(input, 'r') as f, open(output, 'w') as outputF:
         line = line[:-1] # remove eol
         if re.match(r'^ *@import url', line):
             url = re.sub(r'^ *@import url\(([^)]*)\);.*', r'\1', line)
-            remote = readURL(url)
+            remote = readURL(url).decode('utf-8')
             def processUrlInRemote(x):
                 fonturl = x.group(1)
                 format = x.group(2)
@@ -26,8 +26,8 @@ with open(input, 'r') as f, open(output, 'w') as outputF:
                 fontfilecontent = readURL(fonturl)
                 targetFile = 'local-fonts/'+fontfilename
                 writeFile(targetFile, fontfilecontent)
-                print 'GOT:'+fontfilename
-                return 'url(%s) %s, %s' % (targetFile, format, x.group(0))
+                print('GOT:'+fontfilename)
+                return 'url({}) {}, {}'.format(targetFile, format, x.group(0))
             updatedRemote = re.sub(r'url\(([^)]*)\) +(format\([^)]*\))', processUrlInRemote, remote)
             outputF.write(updatedRemote)
         else:
